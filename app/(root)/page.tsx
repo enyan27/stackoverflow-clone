c534a2de2a556efd756af16881c158a732f73571
enyan27 <truongdat2325@gmail.com>
2025-04-16 11:50:26 +0700

    Implement Log Out and Show User Info on Home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 
-import { auth } from "@/auth";
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
@@ -53,11 +52,6 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-
-  // -------------------------- TEST ---------------------------:
-  const session = await auth();
-  console.log("Session: ", session);
-
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
