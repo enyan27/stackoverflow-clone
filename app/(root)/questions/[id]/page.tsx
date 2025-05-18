3edda067162021e244dc16f92a2efef8b1a115a8
enyan27 <truongdat2325@gmail.com>
2025-05-18 09:39:42 +0700

    Integrate Filters Everywhere

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -17,8 +17,9 @@ import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { hasVoted } from "@/lib/actions/vote.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
 
-const QuestionDetails = async ({ params }: RouteParams) => {
+const QuestionDetails = async ({ params, searchParams }: RouteParams) => {
     const { id } = await params;
+    const { page, pageSize, filter } = await searchParams;
 
     // Inc views + Get question[id]
     const [_, { success, data: question }] = await Promise.all([
@@ -35,8 +36,8 @@ const QuestionDetails = async ({ params }: RouteParams) => {
         error: answersError,
     } = await getAnswers({
         questionId: id,
-        page: 1,
-        pageSize: 10,
+        page: Number(page) || 1,
+        pageSize: Number(pageSize) || 10,
         filter: "latest",
     });
     // Check if hasVoted
